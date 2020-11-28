8cf9bd28c8657a54ba2dbac7071ad1aff2517c92
DenisSpirin <s.deniska@mail.ru>
2020-11-28 20:42:56 +0300

    Homework #3: UI Components (модуль homework3uicomponents2) some improvements

--- homework3uicomponents2/src/main/java/ru/denisspirin/homework3uicomponents2/MainActivity.kt
+++ homework3uicomponents2/src/main/java/ru/denisspirin/homework3uicomponents2/MainActivity.kt
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private var fragmentMoviesList: FragmentMoviesList? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentMoviesList = FragmentMoviesList().apply {  }
-
         if (savedInstanceState == null) {
+            fragmentMoviesList = FragmentMoviesList()
             supportFragmentManager.beginTransaction()
-                .add(R.id.main_container, fragmentMoviesList)
+                .add(R.id.main_container, fragmentMoviesList!!, MOVIE_LIST_FRAGMENT_TAG)
                 .commit()
+        } else {
+            fragmentMoviesList = supportFragmentManager.findFragmentByTag(MOVIE_LIST_FRAGMENT_TAG) as? FragmentMoviesList
         }
+    }
+
+    companion object {
+        const val MOVIE_LIST_FRAGMENT_TAG = "FragmentMoviesList"
     }
 }
